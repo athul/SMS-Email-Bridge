aeda934d9b261cf2ac54e49d73fae5bcdddcccca
athul <athul8720@gmail.com>
2020-08-18 00:17:21 +0530

    Update Pipfile and changed unreachable code

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from twilio.rest import Client
 app = FastAPI()
 load_dotenv()
 
-def sendEmail(to:str,subject:str,body:str) -> str:
+
+async def sendEmail(to: str, subject: str, body: str) -> str:
     message = Mail(
         from_email=os.getenv("FROM_EMAIL"),
         subject=subject,
@@ -21,21 +22,22 @@ def sendEmail(to:str,subject:str,body:str) -> str:
         resp = sg.send(message)
         return str(resp.status_code)
     except Exception as exc:
-        return str(exc)
         print(f"Could not send email to {to} due {str(exc)}")
+        return str(exc)
+
 
 @app.post("/hook")
-async def process_sms(From:str = Form(...), Body:str = Form(...)):
+async def process_sms(From: str = Form(...), Body: str = Form(...)):
     """Body of SMS must be like this
     `From` and `Body` will be taken from the Webhook Payload of Twilio SMS Webhook\n
     `"TO:test@example.com,SUB:Any Relevant Subject,MSG:'Any Relevant Message'"`. The MSG part must be enclosed in single quotes(`'`)
     """
     pattern = r"TO:(.*?),SUB:(.*?),MSG:('.*?')"
-    match = re.search(pattern,Body)
+    match = re.search(pattern, Body)
     to_email = match.group(1)
     subject = match.group(2)
-    email_body = match.group(3).replace("'","")
-    email_res = sendEmail(to=to_email,subject=subject,body=email_body)
+    email_body = match.group(3).replace("'", "")
+    email_res = await sendEmail(to=to_email, subject=subject, body=email_body)
     if email_res == "202":
         client = Client()
         message = client.messages.create(
@@ -43,6 +45,6 @@ async def process_sms(From:str = Form(...), Body:str = Form(...)):
             body=f"Email to {to_email} is Sent",
             from_=os.getenv("TWILIO_PHONE_NUMBER")
         )
-        return {"message":f"Successfully Sent Message to {From}"}
+        return {"message": f"Successfully Sent Message to {From}"}
     else:
-        return {"error":f"Unsuccessfull due to {email_res}"}
+        return {"error": f"Unsuccessful due to {email_res}"}
