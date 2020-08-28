dff452bd2dd74db703532481f498622f024b1c28
Athul Cyriac Ajay <athul8720@gmail.com>
2020-08-28 16:43:08 +0000

    Complete Setup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 from dotenv import load_dotenv
 import re
 from fastapi import FastAPI, Form
@@ -6,35 +7,30 @@ import httpx
 
 app = FastAPI()
 load_dotenv()
-print("Env Loaded")
 client = httpx.AsyncClient()
 
+
 async def sendEmail(to: str, subject: str, body: str) -> int:
-    print("Ivide ⚡️")
-    token_header = f"Bearer {os.getenv('SENDGRID_API_KEY')}"
     headers = {
-        'authorization': token_header,
+        'authorization': f"Bearer {os.getenv('SENDGRID_API_KEY')}",
         'content-type': "application/json"
     }
     req = {
-        "personalizations": 
-        [{"to": [{"email": to}]}],
-        "from": {"email": os.getenv(
-        'FROM_EMAIL')}, 
-        "subject": subject, 
+        "personalizations": [{"to": [{"email": to}]}],
+        "from": {"email": os.getenv('FROM_EMAIL')},
+        "subject": subject,
         "content": [{"type": "text/plain", "value": body}]
-        }
+    }
 
-    async with httpx.AsyncClient() as client:
-        resp = await client.post("https://api.sendgrid.com/v3/mail/send", json=req, headers=headers)
+    resp = await client.post("https://api.sendgrid.com/v3/mail/send", json=req, headers=headers)
 
-    print("Email 😅")
     return resp.status_code
 
 
 @app.post("/hook")
-async def process_sms(From: str = Form(...), Body: str = Form(...)):
-    """Body of SMS must be like this
+async def process_sms(From: str = Form(...), Body: str = Form(...)) -> Dict:
+    """
+    Body of SMS must be like this
     `From` and `Body` will be taken from the Webhook Payload of Twilio SMS Webhook\n
     `"TO:test@example.com,SUB:Any Relevant Subject,MSG:'Any Relevant Message'"`. The MSG part must be enclosed in single quotes(`'`)
     """
@@ -43,11 +39,13 @@ async def process_sms(From: str = Form(...), Body: str = Form(...)):
     to_email = match.group(1)
     subject = match.group(2)
     email_body = match.group(3).replace("'", "")
+
     email_res = await sendEmail(to=to_email, subject=subject, body=email_body)
+
     url = f"https://api.twilio.com/2010-04-01/Accounts/{os.getenv('TWILIO_ACCOUNT_SID')}/Messages.json"
     auth = (os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+
     if email_res == 202:
-        print("Email Sent")
         payload = {
             "Body": f"Email to {to_email} is Sent",
             "From": os.getenv('TWILIO_PHONE_NUMBER'),
