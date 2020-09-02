9c1e19554790a831b47689f1a2915052c60f545d
Athul Cyriac Ajay <athul8720@gmail.com>
2020-09-02 21:43:19 +0530

    Changed to Twiml

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import os
-from typing import Dict
+from typing import Dict,List
 from dotenv import load_dotenv
 import re
 from fastapi import FastAPI, Form
 import httpx
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = FastAPI()
 load_dotenv()
 client = httpx.AsyncClient()
 
 
-async def sendEmail(to: str, subject: str, body: str) -> int:
+async def sendEmail(to: List, subject: str, body: str) -> int:
     headers = {
         'authorization': f"Bearer {os.getenv('SENDGRID_API_KEY')}",
         'content-type': "application/json"
     }
     req = {
-        "personalizations": [{"to": [{"email": to}]}],
+        "personalizations": [{"to": to}],
         "from": {"email": os.getenv('FROM_EMAIL')},
         "subject": subject,
         "content": [{"type": "text/plain", "value": body}]
     }
 
     resp = await client.post("https://api.sendgrid.com/v3/mail/send", json=req, headers=headers)
-
+    print(resp.status_code)
     return resp.status_code
 
 
@@ -34,30 +35,25 @@ async def process_sms(From: str = Form(...), Body: str = Form(...)) -> Dict:
     `From` and `Body` will be taken from the Webhook Payload of Twilio SMS Webhook\n
     `"TO:test@example.com,SUB:Any Relevant Subject,MSG:'Any Relevant Message'"`. The MSG part must be enclosed in single quotes(`'`)
     """
-    pattern = r"TO:(.*?),SUB:(.*?),MSG:('.*?')"
+    pattern = r"to:(.*?)sub:(.*),msg:(.*)"
     match = re.search(pattern, Body)
     to_email = match.group(1)
     subject = match.group(2)
-    email_body = match.group(3).replace("'", "")
-
-    email_res = await sendEmail(to=to_email, subject=subject, body=email_body)
-
-    url = f"https://api.twilio.com/2010-04-01/Accounts/{os.getenv('TWILIO_ACCOUNT_SID')}/Messages.json"
-    auth = (os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+    email_body = match.group(3)
+    emails = re.compile("[\w.]+@\w+\.[a-z]{3}").findall(to_email)
+    if len(emails) == 0:
+        return {"msg":"Invalid Email Address(es)"}
+    email_list = [{"email":email} for email in emails]
+    print(email_list)
+    email_res = await sendEmail(to=email_list, subject=subject, body=email_body)
+    print(f"Main Endpoint resp of Email - {email_res}")
+    # url = f"https://api.twilio.com/2010-04-01/Accounts/{os.getenv('TWILIO_ACCOUNT_SID')}/Messages.json"
+    # auth = (os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+    resp = MessagingResponse()
 
     if email_res == 202:
-        payload = {
-            "Body": f"Email to {to_email} is Sent",
-            "From": os.getenv('TWILIO_PHONE_NUMBER'),
-            "To": From
-        }
-        response = await client.post(url, data=payload, auth=auth)
+        resp.message(f"Email to {emails} was successful")
         return {"message": f"Successfully Sent SMS to {From}"}
     else:
-        payload = {
-            "Body": f"Email to {to_email} was unsuccessful",
-            "From": os.getenv('TWILIO_PHONE_NUMBER'),
-            "To": From
-        }
-        response = await client.post(url, data=payload, auth=auth)
+        resp.message(f"Email to {emails} was unsuccessful")
         return {"error": f"Unsuccessful due to {email_res}"}
